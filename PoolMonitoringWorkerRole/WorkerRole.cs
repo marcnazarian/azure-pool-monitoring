e7e57dc9ecf1598cc3fad9c2f759321debbf609c
Marc Nazarian <marc.nazarian@persistent.com>
2015-06-20 01:32:46 +0200

    Add a new project: the PoolSensorEmulator that sends sensor data over the service bus Implemented worker role to read messages from the queue and update the database

--- PoolMonitoringWorkerRole/WorkerRole.cs
+++ PoolMonitoringWorkerRole/WorkerRole.cs
@@ -1,47 +1,108 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Net;
-using System.Threading;
-using Microsoft.ServiceBus;
-using Microsoft.ServiceBus.Messaging;
-using Microsoft.WindowsAzure;
-using Microsoft.WindowsAzure.ServiceRuntime;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright company="Company" file="WorkerRole.cs">
+//   The worker role
+// </copyright>
+// <summary>
+//   The worker role.
+// </summary>
+// 
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace PoolMonitoringWorkerRole
 {
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Threading;
+
+    using Microsoft.ServiceBus;
+    using Microsoft.ServiceBus.Messaging;
+    using Microsoft.WindowsAzure;
+    using Microsoft.WindowsAzure.ServiceRuntime;
+
+    using PoolMonitoringWebRole.Models;
+
+    /// <summary>
+    /// The worker role.
+    /// </summary>
     public class WorkerRole : RoleEntryPoint
     {
-        // The name of your queue
-        const string QueueName = "ProcessingQueue";
+        /// <summary>
+        /// The queue name.
+        /// </summary>
+        private const string QueueName = "ProcessingQueue";
+  
+        /// <summary>
+        /// The client.
+        /// </summary>
+        private QueueClient client;
 
-        // QueueClient is thread-safe. Recommended that you cache 
-        // rather than recreating it on every request
-        QueueClient Client;
-        ManualResetEvent CompletedEvent = new ManualResetEvent(false);
+        /// <summary>
+        /// The completed event.
+        /// </summary>
+        private ManualResetEvent completedEvent = new ManualResetEvent(false);
 
+        /// <summary>
+        /// The run.
+        /// </summary>
         public override void Run()
         {
-            Trace.WriteLine("Starting processing of messages");
+            Console.WriteLine("Starting processing of messages");
+
+            OnMessageOptions options = new OnMessageOptions();
+            options.AutoComplete = false;
+            options.AutoRenewTimeout = TimeSpan.FromMinutes(1);
 
             // Initiates the message pump and callback is invoked for each message that is received, calling close on the client will stop the pump.
-            Client.OnMessage((receivedMessage) =>
-                {
-                    try
+            this.client.OnMessage(
+                receivedMessage =>
                     {
-                        // Process the message
-                        Trace.WriteLine("Processing Service Bus message: " + receivedMessage.SequenceNumber.ToString());
-                    }
-                    catch
-                    {
-                        // Handle any message processing specific exceptions here
-                    }
-                });
+                        try
+                        {
+                            // Process the message
+                            Console.WriteLine("Processing Service Bus message: " + receivedMessage.SequenceNumber.ToString());
 
-            CompletedEvent.WaitOne();
+                            PoolSensorData poolSensorData = new PoolSensorData
+                                                                {
+                                                                    PoolId = (int)receivedMessage.Properties["pool_id"],
+                                                                    Ph = (double)receivedMessage.Properties["ph"],
+                                                                    Temperature = (double)receivedMessage.Properties["temperature"]
+                                                                };
+                            PoolMonitoringContext databaseContext = new PoolMonitoringContext();
+                            var sensorData = databaseContext.PoolSensorDatas.SingleOrDefault(d => d.PoolId.Equals(poolSensorData.PoolId));
+                            if (sensorData == null)
+                            {
+                                databaseContext.PoolSensorDatas.Add(poolSensorData);
+                            }
+                            else
+                            {
+                                sensorData.Ph = poolSensorData.Ph;
+                                sensorData.Temperature = poolSensorData.Temperature;
+                            }
+
+                            databaseContext.SaveChanges();
+
+                            receivedMessage.Complete();
+                        }
+                        catch (Exception exception)
+                        {
+                            Console.WriteLine(exception.Message);
+
+                            // Handle any message processing specific exceptions here
+                            receivedMessage.Abandon();
+                        }
+                    },
+                options);
+
+            this.completedEvent.WaitOne();
         }
 
+        /// <summary>
+        /// The on start.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="bool"/>.
+        /// </returns>
         public override bool OnStart()
         {
             // Set the maximum number of concurrent connections 
@@ -56,15 +117,18 @@ namespace PoolMonitoringWorkerRole
             }
 
             // Initialize the connection to Service Bus Queue
-            Client = QueueClient.CreateFromConnectionString(connectionString, QueueName);
+            this.client = QueueClient.CreateFromConnectionString(connectionString, QueueName);
             return base.OnStart();
         }
 
+        /// <summary>
+        /// The on stop.
+        /// </summary>
         public override void OnStop()
         {
             // Close the connection to Service Bus Queue
-            Client.Close();
-            CompletedEvent.Set();
+            this.client.Close();
+            this.completedEvent.Set();
             base.OnStop();
         }
     }
